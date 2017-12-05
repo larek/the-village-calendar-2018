a69e64e2ebdf48632b1f166e9080f3665ceee4cc
Aleksandr Tikhonov <t9101029991@gmail.com>
2017-12-05 18:33:49 +0300

    create list in loop

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Days from './days.js';
 
 class App extends React.Component{
   constructor(props){
     super(props)
+    this.state = {
+      list: []
+    }
+  }
+
+  componentDidMount(){
+    console.log(Days);
   }
 
   change(e){
-    console.log(e.currentTarget.dataset.lock);
     if(!e.currentTarget.dataset.lock){
       let children = e.currentTarget.children;
       children[0].classList.toggle('transparent');
@@ -18,25 +25,18 @@ class App extends React.Component{
   render(){
     return(
         <div className="stk-grid" data-ce-tag="grid">
-            <div className="stk-grid-col" data-ce-tag="grid-col" data-col-width="2">
-                <div id="cf2" className="shadow" onClick={this.change.bind(this)} data-day='11'>
-                  <img id="front" src="http://reports.larek.pro/api/the-village-calendar-2018/images/day-11.svg" className='img-fluid' />
-                  <img id="back" src="http://reports.larek.pro/api/the-village-calendar-2018/images/day-11-back.svg" className='img-fluid transparent' />
-                </div>
-            </div>
-            <div className="stk-grid-col" data-ce-tag="grid-col" data-col-width="2">
-                <div id="cf2" className="shadow" onClick={this.change.bind(this)} data-day='11'>
-                  <img id="front" src="http://reports.larek.pro/api/the-village-calendar-2018/images/day-12.svg" className='img-fluid' />
-                  <img id="back" src="http://reports.larek.pro/api/the-village-calendar-2018/images/day-11-back.svg" className='img-fluid transparent' />
-                </div>
-            </div>
-            <div className="stk-grid-col stk-grid-col_last" data-ce-tag="grid-col" data-col-width="2">
-                <div id="cf2" className="shadow" onClick={this.change.bind(this)} data-day='11' data-lock>
-                  <img id="front" src="http://reports.larek.pro/api/the-village-calendar-2018/images/day-13.svg" className='img-fluid' />
-                  <img id="back" src="http://reports.larek.pro/api/the-village-calendar-2018/images/day-11-back.svg" className='img-fluid transparent' />
-                  <img src="http://reports.larek.pro/api/the-village-calendar-2018/images/lock.svg" className='lock' alt=""/>
-                </div>
-            </div>
+          {
+            Days.map(item => {
+              return(
+                  <div className="stk-grid-col" data-ce-tag="grid-col" data-col-width="2">
+                      <div id="cf2" className="shadow" onClick={this.change.bind(this)} >
+                        <img id="front" src={"http://reports.larek.pro/api/the-village-calendar-2018/images/day-" + item.day + ".svg"} className='img-fluid' />
+                        <img id="back" src="http://reports.larek.pro/api/the-village-calendar-2018/images/day-1-back.svg" className='img-fluid transparent' />
+                      </div>
+                  </div>
+                );
+            });
+          }
         </div>
       )
   }
