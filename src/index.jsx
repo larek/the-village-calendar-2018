7233781e208c46879916b8d59bda0f760217a45b
Aleksandr Tikhonov <t9101029991@gmail.com>
2017-12-05 16:28:53 +0300

    img urls

--- src/index.jsx
+++ src/index.jsx
@@ -18,21 +18,21 @@ class App extends React.Component{
           <div className="section group">
             <div className="col span_1_of_3">
               <div id="cf2" className="shadow" onClick={this.change.bind(this)} data-day='11'>
-              <img id="front" src="/images/day-11.svg" className='img-fluid' />
-              <img id="back" src="/images/day-11-back.svg" className='img-fluid transparent' />
+              <img id="front" src="http://reports.larek.pro/api/the-village-calendar-2018/images/day-11.svg" className='img-fluid' />
+              <img id="back" src="http://reports.larek.pro/api/the-village-calendar-2018/images/day-11-back.svg" className='img-fluid transparent' />
               </div>
             </div>
             <div className="col span_1_of_3">
               <div id="cf2" className="shadow" data-day='12'>
-              <img id="front" src="/images/day-12.svg" className='img-fluid' />
-              <img id="back" src="/images/day-11-back.svg" className='img-fluid transparent' />
+              <img id="front" src="http://reports.larek.pro/api/the-village-calendar-2018/images/day-12.svg" className='img-fluid' />
+              <img id="back" src="http://reports.larek.pro/api/the-village-calendar-2018/images/day-11-back.svg" className='img-fluid transparent' />
               </div>
             </div>
             <div className="col span_1_of_3">
               <div id="cf2" className="shadow" data-day='13' data-lock>
-              <img id="front" src="/images/day-13.svg" className='img-fluid' />
-              <img id="back" src="/images/day-11-back.svg" className='img-fluid transparent' />
-              <img src="/images/lock.svg" className='lock' alt="" />
+              <img id="front" src="http://reports.larek.pro/api/the-village-calendar-2018/images/day-13.svg" className='img-fluid' />
+              <img id="back" src="http://reports.larek.pro/api/the-village-calendar-2018/images/day-11-back.svg" className='img-fluid transparent' />
+              <img src="http://reports.larek.pro/api/the-village-calendar-2018/images/lock.svg" className='lock' alt="" />
             </div>
           </div>
           </div>
